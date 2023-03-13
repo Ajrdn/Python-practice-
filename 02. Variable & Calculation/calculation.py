779ff6c2ccd2472004d9cb75267d0f1de9a16c1d
Ajrdn <94908806+Ajrdn@users.noreply.github.com>
2023-03-14 08:17:07 +0900

    calculation

---
+++ 02. Variable & Calculation/calculation.py
@@ -0,0 +1,13 @@
+addition = 25 + 1
+subtraction = 21 - 11
+multiplication = 9 * 9
+division = 30 / 5
+remainder = 15 % 4
+power = 2 ** 10
+
+print(addition)
+print(subtraction)
+print(multiplication)
+print(division)
+print(remainder)
+print(power)
