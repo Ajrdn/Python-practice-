f1751db8419fa26750d59d354814b20842d9c456
Ajrdn <94908806+Ajrdn@users.noreply.github.com>
2023-03-19 10:44:50 +0900

    list3

---
+++ 06. List/list3.py
@@ -0,0 +1,27 @@
+numbers = [135, 462, 27, 2753, 234]
+
+print(numbers.index(27))
+
+numbers.extend([9, 10, 11])
+
+print(numbers)
+
+numbers.insert(2, 999)
+
+print(numbers)
+
+numbers.insert(-1, 123)
+
+print(numbers)
+
+numbers.insert(100, 999)
+
+print(numbers)
+
+numbers.sort()
+
+print(numbers)
+
+numbers.reverse()
+
+print(numbers)
