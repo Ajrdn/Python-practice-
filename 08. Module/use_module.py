1984bc83603118f28c984f2e69a1cbbc97ae5efa
Ajrdn <94908806+Ajrdn@users.noreply.github.com>
2023-03-22 08:04:50 +0900

    urlModule

--- 08. Module/use_module.py
+++
@@ -1,7 +0,0 @@
-import my_module
-
-selected = my_module.random_rsp()
-
-print('가위인가요?', my_module.SCISSOR == selected)
-print('바위인가요?', my_module.ROCK == selected)
-print('보인가요?', my_module.PAPER == selected)
