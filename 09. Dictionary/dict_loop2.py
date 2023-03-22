e02a57665447c1d259e12a75ececce9958dd7032
Ajrdn <94908806+Ajrdn@users.noreply.github.com>
2023-03-23 08:10:58 +0900

    dict_loop2

---
+++ 09. Dictionary/dict_loop2.py
@@ -0,0 +1,11 @@
+ages = {
+    'Tod': 35,
+    'Jane': 23,
+    'Paul': 62,
+}
+
+for name in ages.keys():
+    print(f'{name}의 나이는 {ages[name]}세 입니다.')
+
+for name in ages:
+    print(f'{name}의 나이는 {ages[name]}세 입니다.')
